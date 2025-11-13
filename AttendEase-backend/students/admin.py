8f0f7e5d07093943e3e23f397a9f19b56ae8061d
sabyasachiGorai <120822558+sabyasachiGorai@users.noreply.github.com>
2025-11-13 19:35:29 +0530

    fresh start backend

--- AttendEase-backend/students/admin.py
+++
@@ -1,8 +0,0 @@
-# students/admin.py
-from django.contrib import admin
-from .models import Student
-
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "roll_number", "email", "attendance_percentage", "created_at")
-    search_fields = ("name", "roll_number", "email")
--- AttendEase-backend/students/urls.py
+++
@@ -1,8 +0,0 @@
-# students/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("students/", views.StudentListCreateAPIView.as_view(), name="student-list-create"),
-    path("students/<int:pk>/", views.StudentRetrieveUpdateAPIView.as_view(), name="student-detail-update"),
-]
