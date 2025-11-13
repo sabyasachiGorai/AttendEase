acc4c243ae5320d4c7691941f5d616508466cfd3
sabyasachiGorai <120822558+sabyasachiGorai@users.noreply.github.com>
2025-11-14 00:51:54 +0530

    home page JSON response added

--- attendease_backend2/urls.py
+++ attendease_backend2/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 
+def home(request):
+    return JsonResponse({"status": "running", "message": "Attendease API working!"})
+
 urlpatterns = [
+    path("", home),   # <--- root route
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
