22632e50593ee0b2e81d46d749aa40ca6e32b4c7
Sabyasachi <sabyasachigorai01@gamil.com>
2025-11-29 17:51:44 +0530

    prepare project for deployment on Railway

---
+++ account/urls.py
@@ -0,0 +1,21 @@
+
+from django.urls import path
+from .views import (userRegistrationView, userLoginView, UserProfileView, changeUserPasswordView,
+                    userPasswordResetEmailView, userPasswordResetView, StudentRegistrationView,
+                    teacherRegistrationView, TeacherProfileView, StudentProfileView, userLogoutView)
+
+urlpatterns = [
+    path('register/', userRegistrationView.as_view(), name='user-registration'),
+    path('login/', userLoginView.as_view(), name='user-login'),
+    path('logout/', userLogoutView.as_view(), name='user-logout'),
+    path('profile/', UserProfileView.as_view(), name='user-profile'),
+    path('changeUserPassword/', changeUserPasswordView.as_view(), name='changeUser-Password'),
+    path('send-password-reset-email/', userPasswordResetEmailView.as_view(), name='send-password-reset-email'),
+    path('reset/<uid>/<token>/', userPasswordResetView.as_view(), name='password-reset'),
+
+    path('studentRegistration/', StudentRegistrationView.as_view(), name='student-registration'),
+    path('teacherRegistration/', teacherRegistrationView.as_view(), name='teacher-registration'),
+    path('teacherProfile/', TeacherProfileView.as_view(), name='teacher-profile'),
+    path('studentProfile/', StudentProfileView.as_view(), name='student-profile'),
+    
+]
