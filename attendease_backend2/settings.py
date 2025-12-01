2b933774209b4fbe0f7c53a8b3de049bb5cf0e41
Sabyasachi <sabyasachigorai01@gamil.com>
2025-12-02 01:58:30 +0530

    cors updated

--- attendease_backend2/settings.py
+++ attendease_backend2/settings.py
@@ -242,4 +242,10 @@ SIMPLE_JWT = {
 }
 
 
-CORS_ALLOW_ALL_ORIGINS = True   # allow all domains
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
